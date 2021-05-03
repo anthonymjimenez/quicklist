da26aed4d78318d91e79e686b18b5181664869e3
anthonymjimenez <anthonyjim96@gmail.com>
2021-05-03 19:57:26 -0400

    forms completed, adding functionality now

--- client/src/components/add-category.js
+++ client/src/components/add-category.js
@@ -1,5 +1,19 @@
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 const AddCategory = () => {
-  return <>Form goes here. (category)</>;
+  return (
+    <Form>
+      <FormGroup>
+        <Label for="title">Title</Label>
+        <Input
+          type="text"
+          name="text"
+          id="title"
+          placeholder="Create new quicklist"
+        />
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+  );
 };
 
 export default AddCategory;
--- client/src/components/add-item.js
+++ client/src/components/add-item.js
@@ -1,5 +1,43 @@
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { useState } from "react";
+import { Multiselect } from "multiselect-react-dropdown";
 const AddItem = () => {
-  return <>Form goes here.</>;
+  let [categories, setCategories] = useState([]);
+  let quicklist = [
+    {
+      title: "Sneakers",
+      id: "12345",
+    },
+    {
+      title: "Streetwear",
+      id: "54321",
+    },
+  ];
+
+  return (
+    <Form>
+      {console.log("selected", categories)}
+      <FormGroup>
+        <Label for="title">Url</Label>
+        <Input
+          type="text"
+          name="text"
+          id="title"
+          placeholder="enter a url and let quicklist do the rest"
+        />
+      </FormGroup>
+      <Label for="categories">Quick Lists</Label>
+      <FormGroup>
+        <Label for="exampleSelectMulti">Select Multiple</Label>
+        <Multiselect
+          onSelect={setCategories}
+          options={quicklist}
+          displayValue="title"
+        />
+      </FormGroup>
+      <Button>Submit</Button>
+    </Form>
+  );
 };
 
 export default AddItem;
