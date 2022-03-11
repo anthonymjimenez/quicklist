6783476450952f507bf03784715141a31bdc6559
anthonymjimenez <anthonyjim96@gmail.com>
2022-03-11 13:59:04 -0500

    added hover description to item control buttons

--- client/src/containers/item-icons.js
+++ client/src/containers/item-icons.js
@@ -5,47 +5,99 @@ import {
   IoAddSharp,
   IoShareSocialOutline,
 } from "react-icons/io5";
-import React, { useContext } from "react";
+import { Button, Tooltip } from "reactstrap";
+import React, { useContext, useState } from "react";
 import { ItemContext } from "../context/Items/ItemContext";
 
 const ItemIcons = ({ setEdit, setItemModal, setDeleteItemModal, item }) => {
   let { autoUpdateItem } = useContext(ItemContext);
+  let [autoUpdateToggle, setAutoUpdateToggle] = useState(false);
+  let [editToggle, setEditToggle] = useState(false);
+  let [addItemToggle, setAddItemToggle] = useState(false);
+  let [deleteItemToggle, setDeleteItemToggle] = useState(false);
   //
-  return (
-    <div>
-      <button
-        onClick={() => {
-          // autoUpdate(true)
-          autoUpdateItem(item._id);
-        }}
-      >
-        <IoReloadCircleSharp />
-      </button>
-      <button
-        onClick={() => {
-          setEdit((edit) => !edit);
-        }}
-      >
-        <IoPencilSharp />
-      </button>
-      <button
-        onClick={() => {
-          setItemModal(true);
-        }}
-      >
-        <IoAddSharp />
-      </button>
 
-      <button
-        onClick={() => {
-          setDeleteItemModal(true);
-        }}
-      >
-        <IoTrashSharp />
-      </button>
-      <button>
+  const toggle = (setToggle) => {
+    setToggle((toggle) => !toggle);
+  };
+  return (
+    <div className="mt-2">
+      <span href="#" id="TooltipAutoUpdate">
+        <Tooltip
+          placement="right"
+          isOpen={autoUpdateToggle}
+          target="TooltipAutoUpdate"
+          toggle={() => toggle(setAutoUpdateToggle)}
+        >
+          Auto update item
+        </Tooltip>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            // autoUpdate(true)
+            autoUpdateItem(item._id);
+          }}
+        >
+          <IoReloadCircleSharp />
+        </Button>
+      </span>
+      <span href="#" id="TooltipEdit">
+        <Tooltip
+          placement="right"
+          isOpen={editToggle}
+          target="TooltipEdit"
+          toggle={() => toggle(setEditToggle)}
+        >
+          Edit item
+        </Tooltip>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            setEdit((edit) => !edit);
+          }}
+        >
+          <IoPencilSharp />
+        </Button>
+      </span>
+      <span href="#" id="TooltipAddItems">
+        <Tooltip
+          placement="right"
+          isOpen={addItemToggle}
+          target="TooltipAddItems"
+          toggle={() => toggle(setAddItemToggle)}
+        >
+          Add/Remove item from categories
+        </Tooltip>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            setItemModal(true);
+          }}
+        >
+          <IoAddSharp />
+        </Button>
+      </span>
+      <span href="#" id="TooltipDeleteItem">
+        <Tooltip
+          placement="right"
+          isOpen={deleteItemToggle}
+          target="TooltipDeleteItem"
+          toggle={() => toggle(setDeleteItemToggle)}
+        >
+          Delete item
+        </Tooltip>
+        <Button
+          className="mr-2"
+          onClick={() => {
+            setDeleteItemModal(true);
+          }}
+        >
+          <IoTrashSharp />
+        </Button>
+      </span>
+      {/* <Button>
         <IoShareSocialOutline />
-      </button>
+      </Button> */}
     </div>
   );
 };
